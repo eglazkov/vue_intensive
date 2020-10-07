d2eeff9da26c5c246e829a153023e3c917667921
Глазков Егор Александрович (4034447) <eaglazkov@dev.vtb>
2020-10-07 19:55:22 +0300

    php функция создания страницы

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,23 @@
 const $ = require("jquery");
 
-$('h1').fadeOut('slow');
+const getPagesList = () => {
+    $.get('./api', (data) => {
+        data.forEach(file => {
+            $("body").append(`<h1>${file}</h1>`);
+        });
+    }, "JSON");
+};
 
-console.log('safsad');
+getPagesList();
+
+$(".create").click(() => {
+    $("h1").remove();
+    $.post("./api/createNewHtmlPage.php", {
+        name: $("input").val()
+    }, (data) => {
+        getPagesList();
+    })
+    .fail(() => {
+        alert("Такая страница уже существует!")
+    });
+});
